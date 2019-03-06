7324238c496ea7058b024c438c4e04b8069b40d3
Ricardo Valim <ricardo.valim@gmail.com>
2019-03-06 18:09:39 +0000

    delete unecessary folders

--- test2/src/js/redux/star.actions.types.js
+++
@@ -1,4 +0,0 @@
-export const REGISTER = 'REG_STAR'
-export const PUT_FOR_SALE = 'SALE_STAR'
-export const BUY_STAR = 'BUY_STAR'
-export const GET_STAR = 'GET_STAR'
--- test2/src/js/redux/star.store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux'
-import StarReducer from './star.reducer'
-const store = createStore(StarReducer)
-
-store.subscribe(() => {
-    console.log("store changed", store.getState())
-});
-
-export default store
